00f18715aa556524d84d90c60336033a9c9cbacb
BennyDioxide <bennystyang@proton.me>
2024-07-31 21:09:14 +0800

    feat: Nix-ld

---
+++ modules/nix-ld.nix
@@ -0,0 +1,46 @@
+{ pkgs, ... }:
+
+{
+    programs.nix-ld = {
+      enable = true;
+      libraries = with pkgs; [
+        alsa-lib
+        atk
+        cairo
+        cups
+        curl
+        dbus
+        fontconfig
+        freetype
+        fuse2
+        fuse3
+        gdk-pixbuf
+        glib
+        gtk2
+        gtk3
+        harfbuzz
+        icu
+        libGL
+        libappindicator-gtk3
+        libdrm
+        libnotify
+        libpulseaudio
+        librsvg
+        libsoup_3
+        libunwind
+        libxkbcommon
+        mesa
+        openssl
+        pango
+        pipewire
+        stdenv.cc.cc
+        systemd
+        webkitgtk
+        webkitgtk_4_1
+        vulkan-loader
+        vips
+        xdotool
+        zlib
+      ];
+    };
+}
