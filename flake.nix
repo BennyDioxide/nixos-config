a880988485d09a1f7fd1953713bd8d3ffd7954b3
BennyDioxide <bennystyang@proton.me>
2025-01-05 19:37:06 +0800

    refactor: Move subtituters configuration to flake

--- flake.nix
+++ flake.nix
@@ -1,12 +1,32 @@
-{
+# Nix Flake doesn't let me use let-in syntax for some reason
+rec {
   description = "Benny's NixOS configuration";
 
   nixConfig = {
     builders-use-substitutes = true;
     trusted-substituters = [
+      # cache mirror in China
+      # status: https://mirror.sjtu.edu.cn/
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
+      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+
+      "https://cache.garnix.io"
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
+
       "https://hyprland.cachix.org"
+      # "https://anyrun.cachix.org"
+      "https://helix.cachix.org"
+      "https://niri.cachix.org"
+    ];
     trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      # "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+      "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
     ];
   };
 
@@ -65,7 +85,18 @@
           ./configuration.nix
           musnix.nixosModules.musnix
           niri.nixosModules.niri
+          (
+            { pkgs, ... }:
 
+            {
+              nix.settings = {
+                inherit (nixConfig) builders-use-substitutes trusted-public-keys;
+                substituters = nixConfig.trusted-substituters;
+                trusted-users = [ "benny" ];
+              };
+              nixpkgs = pkgs-configuration;
+            }
+          )
           home-manager.nixosModules.home-manager
           {
             home-manager = {
