c2b6c56ffc416892a412200f1599c6d87432c282
BennyDioxide <bennystyang@proton.me>
2025-01-05 19:32:06 +0800

    feat: Tmux

---
+++ home/shell/tmux.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+{
+  programs.tmux = {
+    enable = true;
+    plugins = with pkgs; [
+      {
+        plugin = (import ../pkgs/tmux-plugins).pomodoro-plus;
+        extraConfig = ''
+          set -g @pomodoro_notifications 'on'
+          set -g @pomodoro_sound 'on'
+        '';
+      }
+    ];
+    extraConfig = ''
+      unbind r
+      bind r source-file ~/.tmux.conf
+
+      set -g prefix C-s
+
+      setw -g mode-keys vi
+      setw -g mouse on
+      bind-key h select-pane -L
+      bind-key j select-pane -D
+      bind-key k select-pane -U
+      bind-key l select-pane -R
+
+    '';
+  };
+}
