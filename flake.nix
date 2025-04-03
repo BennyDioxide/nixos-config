759409aab57d864300b02c8d9a80cd4749b55848
BennyDioxide <bennystyang@proton.me>
2025-04-03 16:14:19 +0800

    Remove SJTU from substituters as it timeouts often

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@ rec {
     trusted-substituters = [
       # cache mirror in China
       # status: https://mirror.sjtu.edu.cn/
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
 
       "https://cache.garnix.io"
