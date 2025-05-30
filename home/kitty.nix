91500eb2271bc24d23d1829579245c7f84671e7c
BennyDioxide <bennystyang@proton.me>
2025-05-30 22:27:01 +0800

    fix: Kitty not reading nerd font

--- home/kitty.nix
+++ home/kitty.nix
@@ -4,8 +4,9 @@
   programs.kitty = {
     enable = true;
     themeFile = "Catppuccin-Mocha"; # `kitten themes` for preview
-    font.package = pkgs.nerd-fonts.jetbrains-mono;
-    font.name = "JetBrainsMono Nerd Font";
+    # Kitty has nerd-fonts builtin
+    font.package = pkgs.jetbrains-mono;
+    font.name = "JetBrains Mono";
     font.size = 14;
     keybindings = { };
     environment = { };
