c00444f188ab57133da01ac11afaec4639a00984
BennyDioxide <bennystyang@proton.me>
2025-09-17 20:53:12 +0800

    feat: Remove Immich reverse proxy

--- hosts/benny-nixos/immich.nix
+++ hosts/benny-nixos/immich.nix
@@ -14,19 +14,19 @@
     "render"
   ];
 
-  services.nginx.virtualHosts."immich.bdioxide.me" = {
-    enableACME = true;
-    forceSSL = true;
-    locations."/" = {
-      proxyPass = "http://[::1]:${config.services.immich.port}";
-      proxyWebsockets = true;
-      recommendedProxySettings = true;
-      extraConfig = ''
-        client_max_body_size 50000M;
-        proxy_read_timeout   600s;
-        proxy_send_timeout   600s;
-        send_timeout         600s;
-      '';
-    };
-  };
+  # services.nginx.virtualHosts."immich.bdioxide.me" = {
+  #   enableACME = true;
+  #   forceSSL = true;
+  #   locations."/" = {
+  #     proxyPass = "http://[::1]:${toString config.services.immich.port}";
+  #     proxyWebsockets = true;
+  #     recommendedProxySettings = true;
+  #     extraConfig = ''
+  #       client_max_body_size 50000M;
+  #       proxy_read_timeout   600s;
+  #       proxy_send_timeout   600s;
+  #       send_timeout         600s;
+  #     '';
+  #   };
+  # };
 }
