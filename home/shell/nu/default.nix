d5d4918bafb317e46a8cfc6bbd2490730d1ecec1
BennyDioxide <bennystyang@proton.me>
2025-09-20 22:18:00 +0800

    fix: EDITOR environment unset in Nushell

--- home/shell/nu/default.nix
+++ home/shell/nu/default.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
+{ config, ... }:
 {
   programs.nushell = {
     enable = true;
-    package = pkgs.nushell;
     configFile.source = ./config.nu;
     envFile.source = ./env.nu;
+    extraEnv = ''
+      $env.EDITOR = ${config.home.sessionVariables.EDITOR}
+    '';
     shellAliases = {
       bottles-cli = "flatpak run --command=bottles-cli com.usebottles.bottles";
     };
