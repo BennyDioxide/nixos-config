63cdaccfd033dd9e9b977589c4aa3747502a3237
BennyDioxide <bennystyang@proton.me>
2025-09-20 22:26:33 +0800

    fix: Nushell config cannot find parent level config
    Ahh nix module design (or preventing circular import?)

--- home/shell/nu/default.nix
+++ home/shell/nu/default.nix
@@ -1,11 +1,11 @@
-{ config, ... }:
+{ lib, pkgs, ... }:
 {
   programs.nushell = {
     enable = true;
     configFile.source = ./config.nu;
     envFile.source = ./env.nu;
     extraEnv = ''
-      $env.EDITOR = ${config.home.sessionVariables.EDITOR}
+      $env.EDITOR = "${lib.getExe pkgs.helix}"
     '';
     shellAliases = {
       bottles-cli = "flatpak run --command=bottles-cli com.usebottles.bottles";
