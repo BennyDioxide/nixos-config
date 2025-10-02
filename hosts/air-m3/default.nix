928aa902eecb9694168ed80abc13e70bc9ea0796
BennyDioxide <bennystyang@proton.me>
2025-10-03 00:57:39 +0800

    fix: Emacs Daemon not finding Kitty Terminfo

--- hosts/air-m3/default.nix
+++ hosts/air-m3/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 let
   username = "bennyyang";
@@ -11,6 +11,10 @@ in
 
   services.emacs.enable = true;
   services.emacs.package = pkgs.emacs-macport;
+  launchd.user.agents.emacs.environment.TERMINFO_DIRS =
+    map (path: path + "/share/terminfo") config.environment.profiles
+    ++ [ "/usr/share/terminfo" ];
+  environment.enableAllTerminfo = true;
 
   users.users."${username}".home = "/Users/${username}";
   system.primaryUser = username;
