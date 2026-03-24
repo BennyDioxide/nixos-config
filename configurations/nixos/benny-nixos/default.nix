e33967c2801672640a87f84b10e3377124751c35
BennyDioxide <bennystyang@proton.me>
2026-03-24 20:52:43 +0800

    feat: zswap

--- configurations/nixos/benny-nixos/default.nix
+++ configurations/nixos/benny-nixos/default.nix
@@ -43,6 +43,13 @@ in
   boot = {
     kernelPackages = pkgs.linuxPackages_zen;
     extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
+    kernelParams = [
+      "zswap.enabled=1" # enables zswap
+      "zswap.compressor=zstd" # compression algorithm
+      "zswap.max_pool_percent=60" # maximum percentage of RAM that zswap is allowed to use
+      "zswap.shrinker_enabled=1" # whether to shrink the pool proactively on high memory pressure
+      "zswap.zpool=zsmalloc" # pool allocator
+    ];
     loader = {
       efi = {
         canTouchEfiVariables = true;
